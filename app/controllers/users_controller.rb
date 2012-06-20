595458a873649b4471e64759f423fd82cb578175
Billy Mizrahi <billymizrahi@gmail.com>
2012-06-20 18:27:17 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,15 @@
 #
 
 class User < ActiveRecord::Base
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
